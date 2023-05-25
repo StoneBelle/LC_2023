b3a2f435841adc00455b408a3e7c3cbe426fb691
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-05-24 21:28:05 -0400

    Update 2452_Max_Subsequence_Score.py

--- Problems/2452_Max_Subsequence_Score.py
+++ Problems/2452_Max_Subsequence_Score.py
@@ -1,28 +1,25 @@
 from itertools import combinations
 class Solution(object):
     def maxScore(self, nums1, nums2, k):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :type k: int
-        :rtype: int
-        """
-
-        # Find all possible subsequence
+ 
+        # Enumerate nums1 to track indices
         indices = enumerate(nums1)
+        
+        # Find all possible combinations
         comb = combinations(indices, k)
-        score = []
+        score = 0
+        s = 0
+        x = []
 
-        # Determine index of subsequence
+        # Calculate subseq. score
         for indice in list(comb):
-            # sum of nums1 subseq.
-            s = [i[1] for i in indice]
-            # index of nums1 subseq.
-            x = [i[0] for i in indice]
-            
-            # selected nums2 subseq.
-            min_nums2 = [nums2[y]for y in x]
-            
-            # determine subseq. score
-            score.append(sum(s) * min(min_nums2))
-        return max(score)
+            for i in indice:
+                s += i[1]
+                x.append(i[0])
+                min_nums2 = [nums2[y]for y in x]
+                if score < s * min(min_nums2):
+                    score = s * min(min_nums2)
+            s = 0
+            x = []
+
+        return score

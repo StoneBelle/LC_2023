3defd7bdf483be2618a2bd5166447beea272f366
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-05-29 11:48:01 -0400

    Update 258_Add_Digits.py

--- Easy/258_Add_Digits.py
+++ Easy/258_Add_Digits.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def addDigits(self, num):
+        def separate_digits(n):
+            """Separates digits in given num and return the sum of separated digits."""
+            nums = map(int, str(n))
+            n_sum = sum(nums)
+            return n_sum
+
         # Check if num is 0
         if num == 0:
             return 0
-        # Check if num is divisible by 9
-        elif (num % 9) == 0:
-            return 9
-        # If num is not 0 or divisible by 9 
+
+        # Check if num is single digit
         else:
-            return num % 9 
+            answr = separate_digits(num)
+            while answr > 9:
+                a = separate_digits(answr)
+                answr = a
+            return answr
